9cc9118563a074f0432481aab84d4666b58fc6bf
James Balamuta <balamut2@illinois.edu>
2019-03-15 11:32:39 -0500

    Updated list of R packages in use in CBTF
    Added additional packages used by other STAT
    courses at UIUC.

--- r_centos7_install.sh
+++ r_centos7_install.sh
@@ -108,19 +108,24 @@ cat <<- EOF > rpkg-install.R
 
 # The following are packages used in STAT 385
 pkg_list = c('tidyverse', 'rmarkdown', 'shiny',                                                  # EDA tools
-             'devtools', 'testthat', 'roxygen2', 'profvis', 'RSQLite',                           # Development tools
-             'RcppArmadillo', 'rbenchmark', 'microbenchmark',                                    # C++ packages
-             'zoo', 'xts', 'forecast',                                                           # TS packages
-             'maps', 'maptools', 'mapproj', 'mapdata', 'ggmap',                                  # Mapping packages
-             'GGally', 'ggrepel', 'ggraph',
-             'cowplot', 'gridExtra', 'patchwork',
-             'tidytext',
-             'survey', 'fivethirtyeight', 'nycflights13', 'babynames', 'neiss', 'ggplot2movies', # Data packages
-             'caTools', 'bitops',                                                                # Dependencies that are out of date for rmarkdown
-             'PKI', 'RCurl', 'RJSONIO', 'packrat', 'rstudioapi', 'rsconnect',                    # RSConnect
-             'miniUI')
+            'flexdashboard', 'shinydashboard',
+            'devtools', 'testthat', 'roxygen2', 'profvis', 'RSQLite',                           # Development tools
+            'RcppArmadillo', 'rbenchmark', 'microbenchmark',                                    # C++ packages
+            'zoo', 'xts', 'forecast',                                                           # TS packages
+            'maps', 'maptools', 'mapproj', 'mapdata', 'ggmap',                                  # Mapping packages
+            'GGally', 'ggrepel', 'ggraph', 'gganimate',
+            'cowplot', 'gridExtra', 'patchwork',
+            'tidytext', 'tm',
+            'future', 'doParallel',
+            'data.table',
+            'lubridate',
+            'survey', 'fivethirtyeight', 'nycflights13', 'babynames', 'neiss', 'ggplot2movies', # Data packages
+            'caTools', 'bitops',                                                                # Dependencies that are out of date for rmarkdown
+            'PKI', 'RCurl', 'RJSONIO', 'packrat', 'rstudioapi', 'rsconnect',                    # RSConnect
+            'miniUI')
 
-install.packages(pkg_list, repos = "https://cran.rstudio.com")
+# Ncpus allows for quicker compilation.
+install.packages(pkg_list, repos = "https://cran.rstudio.com", Ncpus = 4)
 
 # Install some data packages on GitHub
 devtools::install_github("kjhealy/socviz")
