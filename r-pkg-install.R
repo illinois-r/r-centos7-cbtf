632ff9b2f7e7a7a54f346d29de12a3acbe7de940
James J Balamuta <balamut2@illinois.edu>
2019-03-22 14:54:01 -0500

    Add GPL licensing to code files.

--- r-pkg-install.R
+++ r-pkg-install.R
@@ -1,5 +1,27 @@
 #! /usr/bin/env RScript
 
+#
+# r-pkg-install.R
+#
+# Copyright (C) 2019 James Joseph Balamuta <balamut2@illinois.edu>
+#
+# Version 2.0.0 -- 03/22/19
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+
 # R Code Ahead to install packages!
 
 # Dynamical detect physical cores...
